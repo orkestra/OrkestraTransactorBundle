ffd442e5069201f2b881a3aad4cb141c6c2ca763
Tyler Sommer <sommertm@gmail.com>
2012-02-22 08:15:46 -0700

    Custom DBAL types are now registered in the bundle's boot method. fixes #622

--- OrkestraTransactorBundle.php
+++ OrkestraTransactorBundle.php
@@ -2,8 +2,15 @@
 
 namespace Orkestra\TransactorBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle,
+    Doctrine\DBAL\Types\Type;
 
 class OrkestraTransactorBundle extends Bundle
 {
+    public function boot()
+    {
+        Type::addType('enum.orkestra.transaction_type', 'Orkestra\Transactor\DBAL\Types\TransactionTypeEnumType');
+        Type::addType('orkestra.month', 'Orkestra\Transactor\DBAL\Types\MonthType');
+        Type::addType('orkestra.year', 'Orkestra\Transactor\DBAL\Types\YearType');
+    }
 }
