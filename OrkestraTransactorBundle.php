f03c82f1e9701334428cf8cad88caee0cbc44084
Tyler Sommer <sommertm@gmail.com>
2012-04-18 12:52:26 -0600

    Added BankAccount AccountType enumeration registration to bundle boot method. fixes #802

--- OrkestraTransactorBundle.php
+++ OrkestraTransactorBundle.php
@@ -10,6 +10,7 @@ class OrkestraTransactorBundle extends Bundle
     public function boot()
     {
         Type::addType('enum.orkestra.transaction_type', 'Orkestra\Transactor\DBAL\Types\TransactionTypeEnumType');
+        Type::addType('enum.orkestra.bank_account_type', 'Orkestra\Transactor\DBAL\Types\AccountTypeEnumType');
         Type::addType('orkestra.month', 'Orkestra\Transactor\DBAL\Types\MonthType');
         Type::addType('orkestra.year', 'Orkestra\Transactor\DBAL\Types\YearType');
     }
