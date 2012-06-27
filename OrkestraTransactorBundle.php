690e611c34dcc837ed277144a1396ccb94f21336
Tyler Sommer <sommertm@gmail.com>
2012-06-27 17:08:03 -0600

    Changed ResultTypeEnumType registration to ResultStatus. fixes #1075

--- OrkestraTransactorBundle.php
+++ OrkestraTransactorBundle.php
@@ -12,10 +12,10 @@ class OrkestraTransactorBundle extends Bundle
 {
     public function boot()
     {
-        Type::addType('enum.orkestra.network_type', 'Orkestra\Transactor\DBAL\Types\NetworkTypeEnumType');
         Type::addType('enum.orkestra.transaction_type', 'Orkestra\Transactor\DBAL\Types\TransactionTypeEnumType');
+        Type::addType('enum.orkestra.network_type', 'Orkestra\Transactor\DBAL\Types\NetworkTypeEnumType');
         Type::addType('enum.orkestra.bank_account_type', 'Orkestra\Transactor\DBAL\Types\AccountTypeEnumType');
-        Type::addType('enum.orkestra.result_type', 'Orkestra\Transactor\DBAL\Types\ResultTypeEnumType');
+        Type::addType('enum.orkestra.result_status', 'Orkestra\Transactor\DBAL\Types\ResultStatusEnumType');
         Type::addType('orkestra.month', 'Orkestra\Transactor\DBAL\Types\MonthType');
         Type::addType('orkestra.year', 'Orkestra\Transactor\DBAL\Types\YearType');
     }
